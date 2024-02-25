6932616cde4d3c8b70dd4fe294c42ce7d23115e0
Tom Johnson <bkhz@thebitcrusher.net>
2024-02-25 22:25:54 +1100

    auto-setup packer if its not setup

--- README.md
+++ README.md
@@ -4,14 +4,6 @@
 
 ### Packer
 
-Install packer somewhere in your `packpath`:
-```bash
-$ git clone --depth 1 https://github.com/wbthomason/packer.nvim\
-    ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-```
-
-Once packer has been installed for the first time, it will manage itself as any other packer plugin.
-
 Start neovim and install plugins:
 ```bash
 :PackerSync
--- lua/BKHZ/packer.lua
+++ lua/BKHZ/packer.lua
@@ -1,5 +1,17 @@
-vim.cmd [[packadd packer.nvim]]
+local ensure_packer = function()
+	local fn = vim.fn
+	local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+		vim.cmd [[packadd packer.nvim]]
+		return true
+	end
+	return false
+end
 
+local packer_bootstrap = ensure_packer()
+
+vim.cmd [[packadd packer.nvim]]
 return require("packer").startup(function(use)
 	-- Packer manages itself
 	use "wbthomason/packer.nvim"
@@ -19,7 +31,9 @@ return require("packer").startup(function(use)
 	use{
 		"nvim-treesitter/nvim-treesitter",
 		run = function()
-			local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+			local ts_update = require("nvim-treesitter.install").update({
+				with_sync = true,
+			})
 		end
 	}
 
@@ -41,4 +55,9 @@ return require("packer").startup(function(use)
 
 	-- Indentation guides
 	use("lukas-reineke/indent-blankline.nvim")
+
+	-- Automatically setup packer after clone
+	if packer_bootstrap then
+		require("packer").sync()
+	end
 end)
