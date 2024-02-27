e27a52370e4bd6e27652c3edbe60dcbe0d98dd05
Tom Johnson <bkhz@thebitcrusher.net>
2024-02-27 17:38:44 +1100

    updated LSP config

--- lua/BKHZ/lazy/lsp.lua
+++ lua/BKHZ/lazy/lsp.lua
@@ -1,23 +1,35 @@
 return {
 	"neovim/nvim-lspconfig",
 
+	event = {
+		"InsertEnter",
+		"CmdlineEnter",
+	},
+
 	dependencies = {
 		"williamboman/mason.nvim",
 		"williamboman/mason-lspconfig.nvim",
 		"hrsh7th/nvim-cmp",
 		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
+		"hrsh7th/cmp-nvim-lsp-signature-help",
 		"folke/neodev.nvim",
+		"L3MON4D3/LuaSnip",
 	},
 
 	config = function()
 		local cmp = require("cmp")
 		local cmp_lsp = require("cmp_nvim_lsp")
+		local lspconfig = require("lspconfig")
 		local capabilities = vim.tbl_deep_extend(
 			"force",
 			{},
 			vim.lsp.protocol.make_client_capabilities(),
 			cmp_lsp.default_capabilities())
 
+		-- Mason
 		require("mason").setup {
 			max_concurrent_installers = 4,
 			ui = {
@@ -30,6 +42,7 @@ return {
 			lspconfig = true,
 		}
 
+		-- Mason LSP providers
 		require("mason-lspconfig").setup {
 			automatic_installation = false,
 			ensure_installed = {
@@ -46,13 +59,12 @@ return {
 			handlers = {
 				-- Default LSP server handler
 				function(server)
-					require("lspconfig")[server].setup {
-						capabilities = capabilities
+					lspconfig[server].setup {
+						capabilities = capabilities,
 					}
 				end,
 
 				["lua_ls"] = function()
-					local lspconfig = require("lspconfig")
 					lspconfig.lua_ls.setup {
 						capabilities = capabilities,
 						settings = {
@@ -67,19 +79,45 @@ return {
 			}
 		}
 
+		local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+		-- Completion
 		cmp.setup {
+			snippet = {
+				expand = function(args)
+					require("luasnip").lsp_expand(args.body)
+				end,
+			},
+
+			formatting = {
+				expandable_indicator = true,
+				fields = { "kind", "abbr", "menu" },
+				format = function(entry, item)
+					item.menu = entry:get_completion_item().detail
+					return item
+				end
+			},
+
+			window = {
+				documentation = cmp.config.window.bordered()
+			},
+
 			sources = cmp.config.sources({
 				{ name = "nvim_lsp" },
+				{ name = "luasnip" },
+				{ name = "nvim_lsp_signature_help" },
 			}, {
 				{ name = "buffer" },
 			}),
 
 			-- Autocompletion key mappings
 			mapping = cmp.mapping.preset.insert({
-				-- Enter key confirms completion item
+				["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+				["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+				["<C-y>"] = cmp.mapping.confirm({ select = true }),
 				["<CR>"] = cmp.mapping.confirm({ select = false }),
-				-- Ctrl + space triggers completion menu
 				["<C-Space>"] = cmp.mapping.complete(),
+				["<C-e>"] = cmp.mapping.abort(),
 			})
 		}
 
