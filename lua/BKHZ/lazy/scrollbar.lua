c0740dfaf94981451efa8dc78479aa7a5473a746
Tom Johnson <bkhz@thebitcrusher.net>
2024-02-28 14:10:38 +1100

    scrollbar auto colors

--- lua/BKHZ/lazy/scrollbar.lua
+++ lua/BKHZ/lazy/scrollbar.lua
@@ -1,23 +1,11 @@
 return {
 	"petertriho/nvim-scrollbar",
 	config = function()
-		local colors = require("tokyonight.colors").setup()
 		require("scrollbar").setup {
 			show = true,
 			show_in_active_only = true,
 			set_highlights = true,
 			hide_if_all_visible = false,
-			handle = {
-				color = colors.bg_highlight,
-			},
-			marks = {
-				Search = { color = colors.orange },
-				Error = { color = colors.error },
-				Warn = { color = colors.warning },
-				Info = { color = colors.info },
-				Hint = { color = colors.hint },
-				Misc = { color = colors.purple },
-			}
 		}
 	end
 }
