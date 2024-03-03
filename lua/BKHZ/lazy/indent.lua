3929062928e71436af100c8413fe60875d108d81
Tom Johnson <bkhz@thebitcrusher.net>
2024-03-03 18:14:58 +1100

    add indentscope plugin

--- lua/BKHZ/lazy/indent.lua
+++ lua/BKHZ/lazy/indent.lua
@@ -1,11 +1,29 @@
 return {
 	"lukas-reineke/indent-blankline.nvim",
+	dependencies = {
+		"echasnovski/mini.indentscope",
+	},
 	config = function()
 		require("ibl").setup {
+			indent = {
+				char = "│",
+			},
 			scope = {
 				-- Disable scope guides
 				enabled = false,
 			}
+		}
+
+		local indentscope = require("mini.indentscope")
+		indentscope.setup {
+			draw = {
+				-- Disable annoying animations
+				animation = indentscope.gen_animation.none(),
+			},
+			options = {
+				try_as_border = true,
+			},
+			symbol = "│",
 		}
 	end
 }
