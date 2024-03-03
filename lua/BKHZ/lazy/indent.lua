9bfd7f92a33710997c113b2f9f958faab9912247
Tom Johnson <bkhz@thebitcrusher.net>
2024-03-03 18:27:07 +1100

    change how indentscope picks up current block

--- lua/BKHZ/lazy/indent.lua
+++ lua/BKHZ/lazy/indent.lua
@@ -21,7 +21,10 @@ return {
 				animation = indentscope.gen_animation.none(),
 			},
 			options = {
+				-- Cursor on method to define block scope
 				try_as_border = true,
+				-- Use row reference instead of column when calculating the block level
+				indent_at_cursor = false,
 			},
 			symbol = "│",
 		}
