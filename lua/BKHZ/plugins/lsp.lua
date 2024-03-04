6c3e36ce6a85d4ccf81d09fc30730b64f15dcfca
Tom Johnson <bkhz@thebitcrusher.net>
2024-03-05 10:08:38 +1100

    add LSP ghost text

--- lua/BKHZ/plugins/lsp.lua
+++ lua/BKHZ/plugins/lsp.lua
@@ -21,6 +21,9 @@ return {
 	},
 
 	config = function()
+		-- Ghost text highlighting
+		vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
+
 		local cmp = require("cmp")
 		local cmp_nvim_lsp = require("cmp_nvim_lsp")
 		local lspconfig = require("lspconfig")
@@ -123,47 +126,75 @@ return {
 			}
 		}
 
-		local cmp_select = { behavior = cmp.SelectBehavior.Select }
+		local cmp_defaults = require("cmp.config.default")()
 
 		-- Completion
 		cmp.setup {
+			-- CMP snippets
 			snippet = {
 				expand = function(args)
 					require("luasnip").lsp_expand(args.body)
 				end,
 			},
 
-			formatting = {
-				expandable_indicator = true,
-				fields = { "kind", "abbr", "menu" },
-				format = function(entry, item)
-					item.menu = entry:get_completion_item().detail
-					return item
-				end
-			},
-
+			-- CMP window
 			window = {
 				completion = cmp.config.window.bordered(),
 				documentation = cmp.config.window.bordered()
 			},
 
+			-- CMP sources
 			sources = cmp.config.sources({
 				{ name = "nvim_lsp" },
+				{ name = "path" },
 				{ name = "luasnip" },
 				{ name = "nvim_lsp_signature_help" },
 			}, {
 				{ name = "buffer" },
 			}),
 
-			-- Autocompletion key mappings
+			-- CMP formatting
+			formatting = {
+				expandable_indicator = true,
+				fields = { "kind", "abbr", "menu" },
+				format = function(entry, item)
+					item.menu = entry:get_completion_item().detail
+					return item
+				end,
+			},
+
+			-- CMP completion
+			completion = {
+				completeopt = "menu,menuone,noinsert",
+			},
+
+			-- CMP key maps
 			mapping = cmp.mapping.preset.insert({
-				["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-				["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-				["<C-y>"] = cmp.mapping.confirm({ select = true }),
-				["<CR>"] = cmp.mapping.confirm({ select = false }),
+				["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+				["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+				["<C-b>"] = cmp.mapping.scroll_docs(-4),
+      			["<C-f>"] = cmp.mapping.scroll_docs(4),
 				["<C-Space>"] = cmp.mapping.complete(),
 				["<C-e>"] = cmp.mapping.abort(),
-			})
+				["<CR>"] = cmp.mapping.confirm({ select = true }),
+				["<S-CR>"] = cmp.mapping.confirm({
+					behavior = cmp.ConfirmBehavior.Replace,
+					select = true,
+				}),
+				["<C-CR>"] = function(fallback)
+					cmp.abort()
+					fallback()
+				end,
+			}),
+
+			-- CMP experiments
+			experimental = {
+				ghost_text = {
+					hl_group = "CmpGhostText",
+				},
+			},
+
+			sorting = cmp_defaults.sorting,
 		}
 
 		-- Diagnostics styling
