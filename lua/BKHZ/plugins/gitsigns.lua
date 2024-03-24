5d9d54b01f895f6f76c8327532d3b37346db373f
Tom Johnson <bkhz@thebitcrusher.net>
2024-03-24 21:30:09 +1100

    update gitsigns configuration

--- lua/BKHZ/plugins/gitsigns.lua
+++ lua/BKHZ/plugins/gitsigns.lua
@@ -2,11 +2,22 @@ return {
 	"lewis6991/gitsigns.nvim",
 	config = function()
 		require("gitsigns").setup {
+			-- Show status symbols in column
 			signcolumn = true,
+			-- Dont change number colors to match status
 			numhl = false,
+			-- Dont change line highlighting to match status
 			linehl = false,
 			-- Inline blame info
 			current_line_blame = false,
+			signs = {
+				add = { text = "▎" },
+				change = { text = "▎" },
+				delete = { text = "" },
+				topdelete = { text = "" },
+				changedelete = { text = "▎" },
+				untracked = { text = "▎" },
+			},
 		}
 	end
 }
