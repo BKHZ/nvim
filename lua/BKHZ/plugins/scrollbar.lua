05fbcbdcbd3c6f046ba7ccde70f08289b1a15277
Tom Johnson <bkhz@thebitcrusher.net>
2024-03-31 03:39:37 +1100

    add scrollbar plugin

---
+++ lua/BKHZ/plugins/scrollbar.lua
@@ -0,0 +1,31 @@
+return {
+    "petertriho/nvim-scrollbar",
+    dependencies = {
+        "catppuccin/nvim",
+    },
+    config = function ()
+        local palette = require("catppuccin.palettes.macchiato")
+        require("scrollbar").setup {
+            show = true,
+            show_in_active_only = false,
+            set_highlights = true,
+            hide_if_all_visible = false,
+            throttle_ms = 100,
+            folds = 1000,
+            handle = {
+                color = palette.overlay0,
+                blend = 50,
+                highlight = "CursorColumn",
+                hide_if_all_visible = true,
+            },
+            handlers = {
+                -- Hide the cursor shape
+                cursor = false,
+                diagnostics = true,
+                gitsigns = true,
+                handle = true,
+                search = false,
+            },
+        }
+    end
+}
