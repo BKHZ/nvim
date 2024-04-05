4abc4279fa2d868deb9d531011b3c405fb43d11a
Tom Johnson <bkhz@thebitcrusher.net>
2024-04-05 15:42:38 +1100

    output windows for neotest

--- lua/BKHZ/plugins/neotest.lua
+++ lua/BKHZ/plugins/neotest.lua
@@ -32,12 +32,24 @@ return {
             },
         }
 
+        -- Run the nearest test
         vim.keymap.set("n", "<leader>tc", function ()
             neotest.run.run()
         end)
 
+        -- Run the tests in current file
         vim.keymap.set("n", "<leader>tf", function ()
             neotest.run.run(vim.fn.expand("%"))
+        end)
+
+        -- Show the tests summary window
+        vim.keymap.set("n", "<leader>ts", function ()
+            neotest.summary.open()
+        end)
+
+        -- Show the tests full output window
+        vim.keymap.set("n", "<leader>to", function()
+            neotest.output.open({ enter = true })
         end)
     end
 }
