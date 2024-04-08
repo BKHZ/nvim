80569b6416e248b90d15eacc60f662f6f996f305
Tom Johnson <bkhz@thebitcrusher.net>
2024-04-09 02:55:42 +1000

    update telescope to 0.1.6

--- lua/BKHZ/plugins/telescope.lua
+++ lua/BKHZ/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
     "nvim-telescope/telescope.nvim",
-    tag = "0.1.5",
+    tag = "0.1.6",
     dependencies = {
         "nvim-telescope/telescope-ui-select.nvim",
         "nvim-lua/plenary.nvim",
