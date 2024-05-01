f3e9209be9016f11fe779d1869a920ba003597e2
Tom Johnson <bkhz@thebitcrusher.net>
2024-05-01 21:45:02 +1000

    chore: swap oil to floating window

--- lua/BKHZ/plugins/oil.lua
+++ lua/BKHZ/plugins/oil.lua
@@ -14,9 +14,19 @@ return {
             },
             view_options = {
                 show_hidden = true,
+            },
+            float = {
+                padding = 2,
+                max_width = 200,
+                max_height = 50,
+                border = "rounded",
+                win_options = {
+                    winblend = 0,
+                }
             }
         }
 
-        vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+        -- Open oil in a float.
+        vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
     end
 }
