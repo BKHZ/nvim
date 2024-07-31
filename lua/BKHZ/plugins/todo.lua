f0dbec839019cd3fd29b5e910b3d3d481e7bba96
Tom Johnson <bkhz@thebitcrusher.net>
2024-07-31 20:11:44 +1000

    chore: todo plugin explicit ripgrep search

--- lua/BKHZ/plugins/todo.lua
+++ lua/BKHZ/plugins/todo.lua
@@ -3,6 +3,20 @@ return {
     config = function ()
         require("todo-comments").setup {
             signs = true,
+            highlight = {
+                -- Multiline support for comments
+                multiline = true
+            },
+            search = {
+                commands = "rg",
+                args = {
+                    "--color=never",
+                    "--no-heading",
+                    "--with-filename",
+                    "--line-number",
+                    "--column"
+                }
+            }
         }
     end
 }
