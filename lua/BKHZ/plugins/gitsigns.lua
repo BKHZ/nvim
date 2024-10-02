2a6a39ead1a69c398ce74131f2c86f6733bd55ec
Tom Johnson <bkhz@thebitcrusher.net>
2024-10-02 14:30:21 +1000

    chore: update gitsigns ghost blame text

--- lua/BKHZ/plugins/gitsigns.lua
+++ lua/BKHZ/plugins/gitsigns.lua
@@ -10,7 +10,16 @@ return {
             -- Dont change line highlighting to match status
             linehl = false,
             -- Inline blame info
-            current_line_blame = false,
+            current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
+            current_line_blame = true,
+            current_line_blame_opts = {
+                virt_text = true,
+                virt_text_pos = 'right_align', -- 'eol' | 'overlay' | 'right_align'
+                delay = 1000,
+                ignore_whitespace = false,
+                virt_text_priority = 100,
+                use_focus = true,
+            },
             signs = {
                 add = { text = "▎" },
                 change = { text = "▎" },
