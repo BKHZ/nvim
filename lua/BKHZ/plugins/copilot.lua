3418baf763630d5dd6f9f2faedb8228c9a8f2571
Tom Johnson <bkhz@thebitcrusher.net>
2024-12-20 02:37:55 +1100

    chore: add copilot config

---
+++ lua/BKHZ/plugins/copilot.lua
@@ -0,0 +1,57 @@
+return {
+    "zbirenbaum/copilot.lua",
+    enabled = true,
+    cmd = 'Copilot',
+    event = 'InsertEnter',
+    opts = {
+        panel = {
+            enabled = false,
+        },
+        suggestion = {
+            enabled = true,
+            auto_trigger = false,
+            keymap = {
+                accept = false,
+                accept_word = '<M-w>',
+                accept_line = '<M-l>',
+                next = '<M-]>',
+                prev = '<M-[>',
+                dismiss = '/',
+            },
+        },
+        filetypes = {
+            markdown = false,
+        }
+    },
+    config = function(_, opts)
+        local cmp = require('cmp')
+        local copilot = require('copilot.suggestion')
+        local luasnip = require('luasnip')
+
+        require('copilot').setup(opts)
+
+        local function set_trigger(trigger)
+            vim.b.copilot_suggestion_auto_trigger = trigger
+            vim.b.copilot_suggestion_hidden = not trigger
+        end
+
+        -- Hide suggestions when the completion menu is open.
+        cmp.event:on('menu_opened', function()
+            if copilot.is_visible() then
+                copilot.dismiss()
+            end
+            set_trigger(false)
+        end)
+
+        -- Disable suggestions when inside a snippet.
+        cmp.event:on('menu_closed', function()
+            set_trigger(not luasnip.expand_or_locally_jumpable())
+        end)
+        vim.api.nvim_create_autocmd('User', {
+            pattern = { 'LuasnipInsertNodeEnter', 'LuasnipInsertNodeLeave' },
+            callback = function()
+                set_trigger(not luasnip.expand_or_locally_jumpable())
+            end,
+        })
+    end
+}
