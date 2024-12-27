d541721d864dd25d52d12f85bde92032b21785eb
Tom Johnson <bkhz@thebitcrusher.net>
2024-12-27 16:15:20 +1100

    chore: add github/copilot.vim stub

--- lua/BKHZ/plugins/copilot.lua
+++ lua/BKHZ/plugins/copilot.lua
@@ -1,63 +1,72 @@
 return {
-    "zbirenbaum/copilot.lua",
-    enabled = true,
-    cmd = 'Copilot',
-    build = ':Copilot auth',
-    event = 'InsertEnter',
-    opts = {
-        panel = {
-            enabled = false,
-        },
-        suggestion = {
-            enabled = true,
-            auto_trigger = false,
-            keymap = {
-                accept = false,
-                accept_word = '<M-w>',
-                accept_line = '<M-l>',
-                next = '<M-]>',
-                prev = '<M-[>',
-                dismiss = '/',
-            },
-        },
-        filetypes = {
-            yaml = false,
-            markdown = false,
-            help = false,
-            gitcommit = false,
-            gitrebase = false,
-            ["."] = false,
-        }
-    },
-    config = function(_, opts)
-        local cmp = require('cmp')
-        local copilot = require('copilot.suggestion')
-        local luasnip = require('luasnip')
-
-        require('copilot').setup(opts)
-
-        local function set_trigger(trigger)
-            vim.b.copilot_suggestion_auto_trigger = trigger
-            vim.b.copilot_suggestion_hidden = not trigger
+    {
+        "github/copilot.vim",
+        enabled = false,
+        config = function ()
+            require('copilot').setup {}
         end
+    },
+    {
+        "zbirenbaum/copilot.lua",
+        enabled = true,
+        cmd = 'Copilot',
+        build = ':Copilot auth',
+        event = 'InsertEnter',
+        opts = {
+            panel = {
+                enabled = false,
+            },
+            suggestion = {
+                enabled = true,
+                auto_trigger = false,
+                keymap = {
+                    accept = false,
+                    accept_word = '<M-w>',
+                    accept_line = '<M-l>',
+                    next = '<M-]>',
+                    prev = '<M-[>',
+                    dismiss = '/',
+                },
+            },
+            filetypes = {
+                yaml = false,
+                markdown = false,
+                help = false,
+                gitcommit = false,
+                gitrebase = false,
+                ["."] = false,
+            }
+        },
+        config = function(_, opts)
+            local cmp = require('cmp')
+            local copilot = require('copilot.suggestion')
+            local luasnip = require('luasnip')
 
-        -- Hide suggestions when the completion menu is open.
-        cmp.event:on('menu_opened', function()
-            if copilot.is_visible() then
-                copilot.dismiss()
+            require('copilot').setup(opts)
+
+            local function set_trigger(trigger)
+                vim.b.copilot_suggestion_auto_trigger = trigger
+                vim.b.copilot_suggestion_hidden = not trigger
             end
-            set_trigger(false)
-        end)
 
-        -- Disable suggestions when inside a snippet.
-        cmp.event:on('menu_closed', function()
-            set_trigger(not luasnip.expand_or_locally_jumpable())
-        end)
-        vim.api.nvim_create_autocmd('User', {
-            pattern = { 'LuasnipInsertNodeEnter', 'LuasnipInsertNodeLeave' },
-            callback = function()
+            -- Hide suggestions when the completion menu is open.
+            cmp.event:on('menu_opened', function()
+                if copilot.is_visible() then
+                    copilot.dismiss()
+                end
+                set_trigger(false)
+            end)
+
+            -- Disable suggestions when inside a snippet.
+            cmp.event:on('menu_closed', function()
                 set_trigger(not luasnip.expand_or_locally_jumpable())
-            end,
-        })
-    end
+            end)
+            vim.api.nvim_create_autocmd('User', {
+                pattern = { 'LuasnipInsertNodeEnter', 'LuasnipInsertNodeLeave' },
+                callback = function()
+                    set_trigger(not luasnip.expand_or_locally_jumpable())
+                end,
+            })
+        end
+    }
 }
