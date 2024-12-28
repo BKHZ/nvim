8385d663f4377a8574ad6e3ac3f94afff956846a
Tom Johnson <bkhz@thebitcrusher.net>
2024-12-28 12:35:56 +1100

    chore: add snacks.nvim (disabled)

--- lua/BKHZ/plugins/indent.lua
+++ lua/BKHZ/plugins/indent.lua
@@ -1,45 +1,66 @@
 return {
-    "lukas-reineke/indent-blankline.nvim",
-    version = false,
-    enabled = true,
-    dependencies = {
-        { "echasnovski/mini.indentscope", version = "*" },
-    },
-    config = function ()
-        require("ibl").setup {
+    {
+        "folke/snacks.nvim",
+        enabled = false,
+        lazy = false,
+        priority = 1000,
+        opts = {
             indent = {
-                char = "│",
-                tab_char = "│",
-            },
-            scope = {
-                -- Disable scope guides
-                enabled = false,
-            },
-            exclude = {
-                filetypes = {
-                    "help",
-                    "alpha",
-                    "Trouble",
-                    "trouble",
-                    "lazy",
-                    "mason",
-                    "notify",
+                enabled = true,
+                char = "┊",
+                only_scope = false,
+                animate = {
+                    enabled = true,
+                    style = "out",
+                    easing = "linear"
                 }
             }
-        }
 
-        local indentscope = require("mini.indentscope")
-        indentscope.setup {
-            draw = {
-                animation = indentscope.gen_animation.exponential(),
-            },
-            options = {
-                -- Cursor on method to define block scope
-                try_as_border = true,
-                -- Use row reference instead of column when calculating the block level
-                indent_at_cursor = false,
-            },
-            symbol = "│",
         }
-    end
+    },
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        version = false,
+        enabled = true,
+        dependencies = {
+            { "echasnovski/mini.indentscope", version = "*" },
+        },
+        config = function ()
+            require("ibl").setup {
+                indent = {
+                    char = "│",
+                    tab_char = "│",
+                },
+                scope = {
+                    -- Disable scope guides
+                    enabled = false,
+                },
+                exclude = {
+                    filetypes = {
+                        "help",
+                        "alpha",
+                        "Trouble",
+                        "trouble",
+                        "lazy",
+                        "mason",
+                        "notify",
+                    }
+                }
+            }
+
+            local indentscope = require("mini.indentscope")
+            indentscope.setup {
+                draw = {
+                    animation = indentscope.gen_animation.exponential(),
+                },
+                options = {
+                    -- Cursor on method to define block scope
+                    try_as_border = true,
+                    -- Use row reference instead of column when calculating the block level
+                    indent_at_cursor = false,
+                },
+                symbol = "│",
+            }
+        end
+    }
 }
