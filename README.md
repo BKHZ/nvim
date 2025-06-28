39ef1ed6f792fbbc044f14ab345f75c4ace57de2
Tom Johnson <bkhz@thebitcrusher.net>
2025-06-28 15:56:55 +1000

    chore: fix colors line length

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # Neovim configuration
 [![Lint](https://github.com/BKHZ/nvim/actions/workflows/lint.yml/badge.svg?branch=main)](https://github.com/BKHZ/nvim/actions/workflows/lint.yml)
 
-![image](https://github.com/user-attachments/assets/dc476306-28bb-4735-abcd-693b44bd33b7)
-
 Plugins are managed via `lazy.nvim` and will automatically synchronize on nvim launch. LSP providers are managed via
 `mason`. Autocompletion is provided by `cmp`.
 
--- lua/BKHZ/plugins/colors.lua
+++ lua/BKHZ/plugins/colors.lua
@@ -119,7 +119,11 @@ return {
                         LazyNormal                 = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
                         MasonNormal                = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
                         -- Dark completion popup menu
-                        Pmenu                      = { fg = theme.ui.shade0, bg = theme.ui.bg_p1, blend = vim.o.pumblend },
+                        Pmenu                      = {
+                            fg = theme.ui.shade0,
+                            bg = theme.ui.bg_p1,
+                            blend = vim.o.pumblend
+                        },
                         PmenuSel                   = { fg = "NONE", bg = theme.ui.bg_p2 },
                         PmenuSbar                  = { bg = theme.ui.bg_m1 },
                         PmenuThumb                 = { bg = theme.ui.bg_p2 },
